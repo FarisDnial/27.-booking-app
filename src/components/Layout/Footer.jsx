b51dc541dc0c88a3e88356a53dc7245ff7133658
FarisDnial <faris.danial.fauzi@gmail.com>
2024-05-20 19:57:44 +0800

    Footer Logo updated

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -4,7 +4,7 @@ export default function FooterWithLogo() {
     return (
         <footer className="w-full bg-black p-8 mt-5">
             <div className="flex flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 bg-black text-center md:justify-between">
-                <img src="src/assets/Lensify3.png" alt="logo-ct" className="h-12 w-auto" />
+                <img src="https://firebasestorage.googleapis.com/v0/b/twitter-app-8a21a.appspot.com/o/posts%2FLensify3.png?alt=media&token=b25e1b92-72ff-48e2-ae06-e01c124f0fe1" alt="logo-ct" className="h-12 w-auto" />
                 <ul className="flex flex-wrap items-center gap-y-2 gap-x-8">
                     <li>
                         <Typography
