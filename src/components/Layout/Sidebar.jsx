39c3e002d6352520482cdefc757339170b942d4b
FarisDnial <faris.danial.fauzi@gmail.com>
2024-05-20 19:41:54 +0800

    update lensify logo

--- src/components/Layout/Sidebar.jsx
+++ src/components/Layout/Sidebar.jsx
@@ -17,7 +17,7 @@ export default function SidebarWithCta() {
         <>
             <Card className="fixed top-0 left-0 right-0 bottom-0 overflow-auto w-full max-w-[20rem] p-4 bg-black border-none" style={{ borderRadius: '0', width: "100%" }}>
                 <div className="mb-2 flex items-center gap-4 py-4 px-5">
-                    <img src="src/assets/Lensify3.png" alt="brand" className="h-15 w-auto" />
+                    <img src="https://firebasestorage.googleapis.com/v0/b/twitter-app-8a21a.appspot.com/o/posts%2FLensify3.jpg?alt=media&token=e72aaf66-342e-486d-849a-a936764f01e2" alt="lensify" className="h-15 w-auto" />
                 </div>
                 <List>
                     <ListItem className="text-white" onClick={navigateToAboutUs}>
